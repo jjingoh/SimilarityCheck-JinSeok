1e3cf47c40168000a4e94dcdc383ca97c5d7ac21
JinSeok Oh <jinseok0.oh@samsung.com>
2023-07-06 11:17:28 +0900

    add checkLength for Similarity Checker

--- SimilarCheck/similar.cpp
+++ SimilarCheck/similar.cpp
@@ -1,5 +1,15 @@
-class SimilarCheck
+#include <string>
+
+using namespace std;
+
+class SimilarityChecker
 {
 public:
-
+	int checkLength(const string& str1, const string& str2)
+	{
+		int long_length = std::max(str1.size(), str2.size());
+		int small_length = std::min(str1.size(), str2.size());
+		int gap = long_length - small_length;
+		return (1 - gap / (double)small_length) * 60;
+	}
 };
--- SimilarTest/similar_test.cpp
+++ SimilarTest/similar_test.cpp
@@ -1,7 +1,29 @@
 #include "pch.h"
 #include "../SimilarCheck/similar.cpp"
+#include <string>
+#include <vector>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+class SimilarityCheckerFixture : public testing::Test
+{
+public:
+	SimilarityChecker checker;
+};
+
+TEST_F(SimilarityCheckerFixture, LengthCheck) {
+	vector < tuple<string, string, int> > cases = {
+		{"ASD", "DSA", 60},
+		{"A", "BB", 0},
+		{"AAABB", "BAA", 20},
+		{"AA", "AAE", 30},
+	};
+	for (auto& item: cases)
+	{
+		string str1, str2;
+		int expected;
+		std::tie(str1, str2, expected) = item;
+		int score = checker.checkLength(str1, str2);
+		EXPECT_EQ(expected, score);
+	}
 }
